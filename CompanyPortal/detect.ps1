9ef32fd866b6e125e6b5ace548212a414b677b08
Nick Brown <nick.brown@jisc.ac.uk>
2022-11-01 18:10:48 +0000

    Added company portal winget app deploy

---
+++ CompanyPortal/detect.ps1
@@ -0,0 +1,10 @@
+    $ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+        if ($ResolveWingetPath){
+               $WingetPath = $ResolveWingetPath[-1].Path
+        }
+    
+    $Winget = $WingetPath + "\winget.exe"
+    $wingettest = &$winget list --id 9WZDNCRFJ3PZ
+    if ($wingettest -like "*9WZDNCRFJ3PZ*"){
+        Write-Host "Found it!"
+    }
